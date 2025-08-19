e8bdfde21f831a1d180d2356457d06cf52c81467
aryan9855 <aryansinghalagra@gmail.com>
2025-08-20 01:03:47 +0530

    .

---
+++ dummy/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+// to do something that is diffrent from the regular flow of the app is called side effect 
+// and we can use useEffect hook to do that
+  const [text, setText] = useState('')
+
+useEffect(() => {
+  console.log('useEffect called')
+},[text ])
+
+
+  const changeHandler = (event) => {
+    console.log(text)
+    setText(event.target.value)
+  }
+
+  return (
+    <div className='App'>
+      <input type="text"  onChange={changeHandler} />
+
+    </div>
+    
+  )
+}
+
+export default App
